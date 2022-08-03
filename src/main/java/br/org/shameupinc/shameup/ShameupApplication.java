a1f253bc06bec3f1fa3978ac550de1fc2c8c4dc6
Luiz <lg.pradoalencar@gmail.com>
2022-08-03 09:50:16 -0300

    Enviando base do projeto

---
+++ src/main/java/br/org/shameupinc/shameup/ShameupApplication.java
@@ -0,0 +1,13 @@
+package br.org.shameupinc.shameup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShameupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShameupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/shameupinc/shameup/ShameupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.shameupinc.shameup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShameupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
