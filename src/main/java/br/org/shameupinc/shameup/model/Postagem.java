a78e0a33b13a87958bf40de77de2b272a1ae69b6
Luiz <lg.pradoalencar@gmail.com>
2022-08-04 10:01:18 -0300

    Criação da model Postagem

---
+++ src/main/java/br/org/shameupinc/shameup/model/Postagem.java
@@ -0,0 +1,81 @@
+package br.org.shameupinc.shameup.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens")
+
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private Boolean anonimo;
+	
+	@NotBlank
+	@Size(min = 3 , max = 2000)
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	@NotBlank
+	@Size(min = 3 , max = 255)
+	private String titulo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Boolean getAnonimo() {
+		return anonimo;
+	}
+
+	public void setAnonimo(Boolean anonimo) {
+		this.anonimo = anonimo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+	
+}
