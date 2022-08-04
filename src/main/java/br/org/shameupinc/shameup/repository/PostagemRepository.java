10d7babcdc071cdbe29d0c6f7ee0e91f763584b9
igoryouiti <igoryouiti@hotmail.com>
2022-08-04 11:32:02 -0300

    Criação de PostagemRepository

---
+++ src/main/java/br/org/shameupinc/shameup/repository/PostagemRepository.java
@@ -0,0 +1,10 @@
+package br.org.shameupinc.shameup.repository;
+
+import br.org.shameupinc.shameup.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+        // livia
+}
