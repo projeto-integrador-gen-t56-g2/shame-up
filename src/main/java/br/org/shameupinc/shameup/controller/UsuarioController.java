7fcfcf9d7d26d7d831c7d384c0317d7346d2f452
Livia <liviaflore@uol.com.br>
2022-08-04 12:06:18 -0300

    Método em UsuarioRepository e CRUD em UsuarioController

---
+++ src/main/java/br/org/shameupinc/shameup/controller/UsuarioController.java
@@ -0,0 +1,73 @@
+package br.org.shameupinc.shameup.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.org.shameupinc.shameup.model.Usuario;
+import br.org.shameupinc.shameup.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+
+	
+	@GetMapping
+    public ResponseEntity<List<Usuario>> getAll(){
+        return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id){
+		return usuarioRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+	}
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario usuario) {
+		return usuarioRepository.findById(usuario.getId()).map(resposta -> ResponseEntity.status(HttpStatus.OK)
+				.body(usuarioRepository.save(usuario))).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		if(usuario.isEmpty())
+			throw new ResponseStatusException (HttpStatus.NOT_FOUND);
+		
+		usuarioRepository.deleteById(id);
+	}
+}
--- src/main/java/br/org/shameupinc/shameup/repository/PostagemRepository.java
+++ src/main/java/br/org/shameupinc/shameup/repository/PostagemRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-        // livia
+    
 }
