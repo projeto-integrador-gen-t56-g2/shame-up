9d8af9cc26a934e2bc79c2fc752812521930cabc
Samy de Simoni <sssouza.samy@gmail.com>
2022-08-04 14:24:53 -0300

    Atualização dos métodos Post e Put para validar a existência de usuarios e categoria

--- src/main/java/br/org/shameupinc/shameup/controller/PostagemController.java
+++ src/main/java/br/org/shameupinc/shameup/controller/PostagemController.java
@@ -3,6 +3,8 @@ package br.org.shameupinc.shameup.controller;
 import br.org.shameupinc.shameup.model.Postagem;
 import br.org.shameupinc.shameup.repository.CategoriaRepository;
 import br.org.shameupinc.shameup.repository.PostagemRepository;
+import br.org.shameupinc.shameup.repository.UsuarioRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,12 @@ public class PostagemController {
 
     @Autowired
     private PostagemRepository postagemRepository;
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll(){
@@ -34,15 +42,30 @@ public class PostagemController {
     }
 
     @PostMapping
-    public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
-    }
-
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		if (categoriaRepository.existsById(postagem.getCategoria().getId()) && usuarioRepository.existsById(postagem.getUsuario().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem)); 
+			
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		
+	}
+	
     @PutMapping
-    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
-        return postagemRepository.findById(postagem.getId()).map(resposta -> ResponseEntity.status(HttpStatus.OK)
-                .body(postagemRepository.save(postagem))).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		if (postagemRepository.existsById(postagem.getId())){
+			
+			if (categoriaRepository.existsById(postagem.getCategoria().getId()) && usuarioRepository.existsById(postagem.getUsuario().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+			
+		}			
+			
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
+	}
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
