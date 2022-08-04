e07d569ff47aa9c153d9cc9c7e1bc215ab258b58
Sthefany <sthefanylins_albuquerque@hotmail.com>
2022-08-04 10:44:12 -0300

    criacao da tabela usuarios

---
+++ src/main/java/br/org/shameupinc/shameup/model/Usuario.java
@@ -0,0 +1,139 @@
+package br.org.shameupinc.shameup.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Size(max= 255)
+	private String nome;
+	
+	@NotBlank
+	@Size(max= 255)
+	private String email;
+	
+	@NotBlank
+	private String tipo;
+	
+	@Size(max=11)
+	private String cpf;
+	
+	@Size(max=14)
+	private String cnpj;
+	
+	@DateTimeFormat(pattern = "dd/MM/yyyy") 
+	private LocalDate data_nascimento;
+	
+	@Size(max=11)
+	private String telefone;
+	
+	@NotBlank
+	@Size(min=6, message= "A senha deve ter no mínimo 6 caracteres")
+	private String senha;
+	
+	private String foto;
+	
+	
+	//getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public LocalDate getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(LocalDate data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+	
+	
+	
+	
+}
