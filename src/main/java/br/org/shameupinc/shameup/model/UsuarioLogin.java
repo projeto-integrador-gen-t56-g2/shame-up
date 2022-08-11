a372214b03b5af217afd621e7f1f63fd853d2ad6
Pedro <pedrocsca@gmail.com>
2022-08-11 10:16:31 -0300

    Adicionando classe UsuarioLogin

---
+++ src/main/java/br/org/shameupinc/shameup/model/UsuarioLogin.java
@@ -0,0 +1,78 @@
+package br.org.shameupinc.shameup.model;
+
+import java.sql.Date;
+
+public class UsuarioLogin {
+	private Long id;
+	private String nome;
+	private String foto;
+	private String senha;
+	private String token;
+	private String cpf;
+	private String email;
+	private String cnpj;
+	private Date data_nascimento;
+	private String tipo;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+}
